b894b489fb8a491f3d17b9f916eaa534275a7eb5
Imanov-Sabutay <mister.sabutay@mail.ru>
2025-12-25 20:53:44 +0300

    Test

---
+++ tasks/sabutay_a_radixSortDoubleWithMerge/common/include/common.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+
+#include "task/include/task.hpp"
+
+namespace sabutay_a_radixSortDoubleWithMerge {
+
+using InType = int;
+using OutType = int;
+using TestType = std::tuple<int, std::string>;
+using BaseTask = ppc::task::Task<InType, OutType>;
+
+
+}  // namespace sabutay_a_radixSortDoubleWithMerge
